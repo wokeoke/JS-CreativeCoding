e713b4f41a15f34d6ca186c78ab0025147deb069
wokeoke <woke.oke@gmail.com>
2022-04-24 00:25:59 +0700

    Angles

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -4,6 +4,10 @@ const settings = {
   dimensions: [1080, 1080],
 };
 
+const degToRod = (degrees) => {
+  return (degrees / 180) * Math.PI;
+};
+
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
@@ -11,19 +15,32 @@ const sketch = () => {
 
     context.fillStyle = 'black';
 
-    const x = width * 0.5;
-    const y = height * 0.5;
-    const w = width * 0.3;
-    const h = height * 0.3;
+    const cx = width * 0.5;
+    const cy = height * 0.5;
 
-    context.save();
-    context.translate(x, y);
-    context.rotate(0.3);
+    const w = width * 0.01;
+    const h = height * 0.1;
+    let x, y;
 
-    context.beginPath();
-    context.rect(-w * 0.5, -h * 0.5, w, h);
-    context.fill();
-    context.restore();
+    const num = 12;
+    const radius = width * 0.3;
+
+    for (let i = 0; i < num; i++) {
+      const slice = degToRod(360 / num);
+      const angle = slice * i;
+
+      x = cx + radius * Math.sin(angle);
+      y = cy + radius * Math.cos(angle);
+
+      context.save();
+      context.translate(x, y);
+      context.rotate(-angle);
+
+      context.beginPath();
+      context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.fill();
+      context.restore();
+    }
   };
 };
 
