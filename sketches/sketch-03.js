383dfac9cfcf85efab1c55c61bd6265d4ec857eb
wokeoke <woke.oke@gmail.com>
2022-04-24 01:36:46 +0700

    Animating Agents

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -3,7 +3,14 @@ const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [1080, 1080],
+  animate: true,
 };
+
+// const animate = () => {
+//   console.log('Animation Frame');
+//   requestAnimationFrame(animate);
+// };
+// animate();
 
 const sketch = ({ context, width, height }) => {
   const agents = [];
@@ -19,21 +26,17 @@ const sketch = ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    // const point = { x: 800, y: 400, radius: 10 };
-    // const agentA = new Agent(800, 400);
-    // const agentB = new Agent(300, 700);
-
-    // agentA.draw(context);
-    // agentB.draw(context);
     agents.forEach((agent) => {
+      agent.update();
       agent.draw(context);
+      agent.bounce(width, height);
     });
   };
 };
 
 canvasSketch(sketch, settings);
 
-class Point {
+class Vector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
@@ -42,14 +45,34 @@ class Point {
 
 class Agent {
   constructor(x, y) {
-    this.pos = new Point(x, y);
-    this.radius = 10;
+    this.pos = new Vector(x, y);
+    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
+    this.radius = random.range(4, 12);
+  }
+
+  bounce(width, height) {
+    if (this.pos.x <= 0 || this.pos.x >= width) this.vel.x *= -1;
+    if (this.pos.y <= 0 || this.pos.y >= height) this.vel.y *= -1;
+  }
+
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
   }
 
   draw(context) {
+    // context.fillStyle = 'black';
+
+    context.save();
+    context.translate(this.pos.x, this.pos.y);
+
+    context.lineWidth = 4;
+
     context.beginPath();
-    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
-    context.fillStyle = 'black';
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
     context.fill();
+    context.stroke();
+
+    context.restore();
   }
 }
