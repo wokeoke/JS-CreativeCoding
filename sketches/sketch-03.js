238d1c05008cc26db6aa914abf503733f0afad8a
wokeoke <woke.oke@gmail.com>
2022-04-24 01:26:59 +0700

    Objects and Classes

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -1,14 +1,55 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [1080, 1080],
 };
 
-const sketch = () => {
+const sketch = ({ context, width, height }) => {
+  const agents = [];
+
+  for (let i = 0; i < 40; i++) {
+    const x = random.range(0, width);
+    const y = random.range(0, height);
+
+    agents.push(new Agent(x, y));
+  }
+
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    // const point = { x: 800, y: 400, radius: 10 };
+    // const agentA = new Agent(800, 400);
+    // const agentB = new Agent(300, 700);
+
+    // agentA.draw(context);
+    // agentB.draw(context);
+    agents.forEach((agent) => {
+      agent.draw(context);
+    });
   };
 };
 
 canvasSketch(sketch, settings);
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Agent {
+  constructor(x, y) {
+    this.pos = new Point(x, y);
+    this.radius = 10;
+  }
+
+  draw(context) {
+    context.beginPath();
+    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
+    context.fillStyle = 'black';
+    context.fill();
+  }
+}
